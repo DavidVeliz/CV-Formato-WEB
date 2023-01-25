17d62b4780adbfed2b0ad980f6e6cf94221733ca
DavidVeliz <122913443+DavidVeliz@users.noreply.github.com>
2023-01-25 16:19:40 -0300

    Se carga proyecto

---
+++ js/Funcion.js
@@ -0,0 +1,42 @@
+document.getElementById('claro').addEventListener('click',function () {
+    document.body.style.backgroundColor = 'cyan';
+    document.body.style.color = 'black';
+    document.getElementById('Uni').style.color = 'black';
+    document.getElementById('exp').style.color = 'black';
+    document.getElementById('info').style.color = 'black';
+    document.getElementById('egg').style.color = 'black';
+    document.getElementById('ap').style.color = 'black';
+    document.getElementById('curso').style.color= 'white';
+    document.getElementById('curso1').style.color= 'white';
+    document.getElementById('Boton_oscuro').style.backgroundColor = 'cyan';
+    document.getElementById('Boton_oscuro').style.color = 'black'
+    document.getElementById('Boton_claro').style.backgroundColor = 'cyan';
+    document.getElementById('Boton_claro').style.color = 'black'
+    document.getElementById('Boton_claro').style.border = 'none'
+    document.getElementById('Boton_oscuro').style.border = 'none'
+    
+    
+    
+})
+
+document.getElementById('oscuro').addEventListener('click',function () {
+    document.body.style.backgroundColor = 'rgb(1, 1, 7';
+    document.body.style.color = 'aliceblue';
+    document.getElementById('Uni').style.color = 'white';
+    document.getElementById('exp').style.color = 'cyan';
+    document.getElementById('info').style.color = 'cyan';
+    document.getElementById('egg').style.color = 'white';
+    document.getElementById('ap').style.color = 'white';
+    document.getElementById('curso').style.color= 'cyan';
+    document.getElementById('curso1').style.color= 'cyan';
+    document.getElementById('Boton_oscuro').style.backgroundColor = 'black';
+    document.getElementById('Boton_oscuro').style.color = 'white';
+    document.getElementById('Boton_claro').style.backgroundColor = 'black';
+    document.getElementById('Boton_claro').style.color = 'white';
+    document.getElementById('Boton_claro').style.border = 'none'
+    document.getElementById('Boton_oscuro').style.border = 'none'
+})
+    document.getElementById('Boton_claro').style.backgroundColor = 'black';
+    document.getElementById('Boton_claro').style.color = 'white';
+    document.getElementById('Boton_oscuro').style.backgroundColor = 'black';
+    document.getElementById('Boton_oscuro').style.color = 'white';
